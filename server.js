f3dfbd3bb26d6711a1211de808604d2e62880fa6
dipak chavan <dc741094@gmail.com>
2026-02-16 15:33:17 +0530

    adde new2

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: "https://q-frontend-rose.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 
 /* ===== Database ===== */
 ConnectDB();
